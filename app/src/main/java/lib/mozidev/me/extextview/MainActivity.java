8c01e19791fbca8ffd780b6d4a392b3b18e4e289
zijing07 <pirazhao@gmail.com>
2018-04-18 16:46:55 +0800

    default mode basically ok

--- app/src/main/java/lib/mozidev/me/extextview/MainActivity.java
+++ app/src/main/java/lib/mozidev/me/extextview/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
             "because we can\n" +
             "For the good of all of us.\n" +
             "Except the ones who are dead.";
+    private static final String VERY_LONG_TEXT = "Early work on the Aperture Science Handheld Portal Device began; the early version, called the Aperture Science Portable Quantum Tunneling Device, proved to be too bulky for effective use, while poor surface conductors for the portals often caused mangling or death of the test subjects when they tried to use the portals.";
 
     private StrikeThroughPainting strikeThroughPainting = null;
 
@@ -30,14 +31,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 tv.setText(SINGLE_LINE_TEXT);
-                strikeThroughPainting.strikeThrough();
+                strikeThroughPainting.strokeWidth(5).strikeThrough();
             }
         });
 
         findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                tv.setText(MULTIPLE_LINES_TEXT);
+                tv.setText(VERY_LONG_TEXT);
                 strikeThroughPainting
                         .mode(StrikeThroughPainting.MODE_DEFAULT)
                         .strikeThrough();
@@ -49,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 tv.setText(MULTIPLE_LINES_TEXT);
                 strikeThroughPainting
-                        .mode(StrikeThroughPainting.MODE_ALL_LINE_TOGETHER)
+                        .mode(StrikeThroughPainting.MODE_DEFAULT)
                         .strikeThrough();
             }
         });
--- extendedtextview/src/main/java/lib/mozidev/me/extendedtextview/StrikeThroughPainting.java
+++ extendedtextview/src/main/java/lib/mozidev/me/extendedtextview/StrikeThroughPainting.java
@@ -6,8 +6,13 @@ import android.animation.ValueAnimator;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.text.Layout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * created by zijing on 17/04/2018
@@ -28,15 +33,17 @@ public class StrikeThroughPainting implements IPainting {
      */
     public static final int MODE_ALL_LINE_TOGETHER = 1;
 
+    private static final float STRIKE_THROUGH_POSITION = 0.7F;
     /**
-     * Because the existence of `font padding`, adjust the strike through line position
-     * from 0.5 to 0.6, to make the line in the center of texts.
+     * Because the `first line top padding` is smaller, adjust the strike through
+     * line position from 0.7 to 0.6, to make the line in the center of texts.
      */
-    private static final float STRIKE_THROUGH_POSITION = 0.6F;
+    private static final float STRIKE_THROUGH_FIRST_LINE_OFFSET = 0.1F;
     private static final float STRIKE_THROUGH_STROKE_WIDTH = 2F;
     private static final int STRIKE_THROUGH_COLOR = Color.BLACK;
     private static final long STRIKE_THROUGH_TOTAL_TIME = 1_000L;
     private static final int STRIKE_THROUGH_MODE = MODE_DEFAULT;
+    private static final boolean STRIKE_THROUGH_CUT_TEXT_EDGE = true;
 
     @NonNull
     private ExtendedTextView targetView;
@@ -44,13 +51,17 @@ public class StrikeThroughPainting implements IPainting {
     private StrikeThroughPaintingCallback strikeThroughPaintingCallback = null;
     private Paint paint = new Paint();
     private boolean drawStrikeThrough = false;
+    private float textHeight = 0F;
 
     private float strikeThroughProgress = 0F;
     private float strikeThroughPosition = STRIKE_THROUGH_POSITION;
+    private float strikeThroughFirstLinePosition = STRIKE_THROUGH_POSITION -
+            STRIKE_THROUGH_FIRST_LINE_OFFSET;
     private int strikeThroughColor = STRIKE_THROUGH_COLOR;
     private float strikeThroughStrokeWidth = STRIKE_THROUGH_STROKE_WIDTH;
     private long strikeThroughTotalTime = STRIKE_THROUGH_TOTAL_TIME;
     private int strikeThroughMode = STRIKE_THROUGH_MODE;
+    private boolean strikeThroughCutTextEdge = STRIKE_THROUGH_CUT_TEXT_EDGE;
 
     public StrikeThroughPainting(@NonNull ExtendedTextView targetView) {
         this.targetView =targetView;
@@ -62,11 +73,24 @@ public class StrikeThroughPainting implements IPainting {
     /**
      * Set strike through line position
      * @param percentageOfHeight set position of the drawing line, percentage marks the
-     *                           offset from the top of the view
+     *                           offset from the top of the line
      * @return this
      */
     public StrikeThroughPainting linePosition(float percentageOfHeight) {
         this.strikeThroughPosition = percentageOfHeight;
+        return this;
+    }
+
+    /**
+     * Set the strike through line position of the first line. Because the
+     * `first line top padding` is smaller, the first line's strike through
+     * line needs to be higher than other lines.
+     * @param percentageOfHeight set position of the drawing line, percentage marks the
+     *                           offset from the top of the line
+     * @return this
+     */
+    public StrikeThroughPainting firstLinePosition(float percentageOfHeight) {
+        this.strikeThroughFirstLinePosition = percentageOfHeight;
         return this;
     }
 
@@ -124,9 +148,183 @@ public class StrikeThroughPainting implements IPainting {
     }
 
     /**
+     * Set if the strike through line cut the text edge, or fill the full width
+     * @param cutEdge whether cut text edge
+     * @return this
+     */
+    public StrikeThroughPainting cutTextEdge(boolean cutEdge) {
+        this.strikeThroughCutTextEdge = cutEdge;
+        return this;
+    }
+
+    /**
      * Start strike through animation
      */
     public void strikeThrough() {
+        targetView.post(new Runnable() {
+            @Override
+            public void run() {
+                prepareAnimation();
+                startAnimation();
+            }
+        });
+    }
+
+    /**
+     * Dismiss the strikeThrough line
+     */
+    public void clearStrikeThrough() {
+        drawStrikeThrough = false;
+        strikeThroughProgress = 0F;
+        targetView.invalidate();
+    }
+
+    private Paint rectPaint = new Paint();
+
+    @Override
+    public void onDraw(@NonNull Canvas canvas) {
+
+//        rectPaint.setColor(Color.BLUE);
+//        if (lineRects != null) {
+//            for (Rect rect : lineRects) {
+//                canvas.drawLine(rect.left, rect.bottom, rect.right, rect.bottom, rectPaint);
+//            }
+//        }
+//        rectPaint.setColor(Color.RED);
+//        if (lineRects != null) {
+//            for (Rect rect : lineRects) {
+//                canvas.drawLine(rect.left, rect.top, rect.right, rect.top, rectPaint);
+//            }
+//        }
+
+        if (!drawStrikeThrough || textHeight == 0F) {
+            return ;
+        }
+        canvas.save();
+
+        switch (strikeThroughMode) {
+            case MODE_DEFAULT:
+                drawDefault(canvas);
+                break;
+            case MODE_ALL_LINE_TOGETHER:
+                break;
+        }
+
+        canvas.restore();
+    }
+
+    private int maxDrawingLineNum(float animCoveredDistance) {
+        float tmpDistance = 0F;
+        if (lineRects == null) {
+            return -1;
+        }
+        for (int i = 0; i < lineRects.size(); ++i) {
+            Rect rect = lineRects.get(i);
+            tmpDistance += (float) rect.width();
+            if (tmpDistance >= animCoveredDistance) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private float drawStrikeThroughLine(@NonNull Canvas canvas, int lineIndex) {
+        return drawStrikeThroughLine(canvas, lineIndex, -1);
+    }
+
+    /**
+     * The function that really draws the line
+     * @param canvas Canvas to draw
+     * @param lineIndex The line to draw strike through on
+     * @param distance How long the strike through line should be, -1 indicates a full line draw
+     * @return The length of strike through line drawn
+     */
+    private float drawStrikeThroughLine(@NonNull Canvas canvas, int lineIndex, float distance) {
+        Rect rect = lineRects.get(lineIndex);
+        float linePosition = lineIndex == 0 ?
+                strikeThroughFirstLinePosition : strikeThroughPosition;
+        float lineHeightOffset = targetView.getLineHeight() * lineIndex
+                + textHeight * linePosition;
+        float howFarToGo = distance == -1 ? rect.width() : distance;
+        canvas.drawLine(rect.left, lineHeightOffset, howFarToGo, lineHeightOffset, paint);
+        return howFarToGo;
+    }
+
+    private void drawDefault(@NonNull Canvas canvas) {
+        float animCoveredDistance = animTotalDistance * strikeThroughProgress;
+        int maxDrawingLineNum = maxDrawingLineNum(animCoveredDistance);
+
+        if (maxDrawingLineNum < 0) {
+            return ;
+        }
+
+        float drawnDistance = 0F;
+        // draw full line strike through
+        for (int i = 0; i < maxDrawingLineNum; ++i) {
+            drawnDistance += drawStrikeThroughLine(canvas, i);
+        }
+
+        // last line strike through
+        float lastLineDrawDistance = animCoveredDistance - drawnDistance;
+        drawStrikeThroughLine(canvas, maxDrawingLineNum, lastLineDrawDistance);
+    }
+
+    private int animTotalDistance = 0;
+    private List<Rect> lineRects;
+
+    private void fillLineRects() {
+        int lineCount = targetView.getLineCount();
+        lineRects = new ArrayList<>();
+        Layout layout = targetView.getLayout();
+        for (int i = 0; i < lineCount; ++i) {
+            Rect rect = new Rect();
+            if (strikeThroughCutTextEdge) {
+                rect.set(
+                        (int) layout.getLineLeft(i),
+                        layout.getLineTop(i),
+                        (int) layout.getLineRight(i),
+                        layout.getLineBottom(i)
+                );
+            } else {
+                targetView.getLineBounds(i, rect);
+            }
+            lineRects.add(rect);
+        }
+    }
+
+    private void calcAnimTotalDistance() {
+        animTotalDistance = 0;
+        if (lineRects == null) {
+            return ;
+        }
+        for (Rect rect: lineRects) {
+            animTotalDistance += rect.width();
+        }
+    }
+
+    private void getLineSpacing() {
+        textHeight = 0F;
+        Paint.FontMetrics fm = targetView.getPaint().getFontMetrics();
+        if (fm == null) {
+            return ;
+        }
+        textHeight = fm.bottom - fm.top;
+    }
+
+    private void prepareAnimation() {
+        getLineSpacing();
+        fillLineRects();
+        switch (strikeThroughMode) {
+            case MODE_DEFAULT:
+                calcAnimTotalDistance();
+                break;
+            case MODE_ALL_LINE_TOGETHER:
+                // ignore
+                break;
+        }
+    }
+
+    private void startAnimation() {
         strikeThroughProgress = 0F;
         drawStrikeThrough = true;
         ValueAnimator animator = ValueAnimator.ofFloat(0F, 1F);
@@ -148,29 +346,6 @@ public class StrikeThroughPainting implements IPainting {
         });
         animator.setDuration(strikeThroughTotalTime);
         animator.start();
-    }
-
-    /**
-     * Dismiss the strikeThrough line
-     */
-    public void clearStrikeThrough() {
-        drawStrikeThrough = false;
-        strikeThroughProgress = 0F;
-        targetView.invalidate();
-    }
-
-    @Override
-    public void onDraw(@NonNull Canvas canvas) {
-        if (!drawStrikeThrough) {
-            return ;
-        }
-        canvas.save();
-
-        float y = targetView.getLineHeight() * strikeThroughPosition;
-        float endX = targetView.getMeasuredWidth() * strikeThroughProgress;
-        canvas.drawLine(0, y, endX, y, paint);
-
-        canvas.restore();
     }
 
     public interface StrikeThroughPaintingCallback {
